b882c0147f86059e514c986a7de13c40ebca573f
이석범 <seokbeom0912@gmail.com>
2026-02-13 22:39:35 +0900

    Fix: 트랜잭션 추가

--- src/main/java/flipnote/group/application/service/DeleteGroupService.java
+++ src/main/java/flipnote/group/application/service/DeleteGroupService.java
@@ -1,6 +1,7 @@
 package flipnote.group.application.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import flipnote.group.application.port.in.DeleteGroupUseCase;
 import flipnote.group.application.port.in.command.DeleteGroupCommand;
@@ -14,6 +15,7 @@ public class DeleteGroupService implements DeleteGroupUseCase {
 	private final GroupRepositoryPort groupRepository;
 
 	@Override
+	@Transactional
 	public void deleteGroup(DeleteGroupCommand cmd) {
 		groupRepository.delete(cmd.groupId());
 	}
