9d063d7da94a634fc349eaec9dd4c46327c1184c
이석범 <seokbeom0912@gmail.com>
2026-02-27 23:35:06 +0900

    Merge: 충돌 해결

--- src/main/java/flipnote/group/infrastructure/persistence/jpa/GroupMemberRepository.java
+++ src/main/java/flipnote/group/infrastructure/persistence/jpa/GroupMemberRepository.java
@@ -3,6 +3,7 @@ package flipnote.group.infrastructure.persistence.jpa;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import flipnote.group.adapter.out.entity.GroupMemberEntity;
 
