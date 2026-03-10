56221f1ffd4350d8c9c36ab67c1c77c074666f81
이석범 <seokbeom0912@gmail.com>
2026-03-10 21:16:31 +0900

    Feat: 그룹 존재 체크 추가

--- src/main/java/flipnote/group/application/service/CheckUserInGroupService.java
+++ src/main/java/flipnote/group/application/service/CheckUserInGroupService.java
@@ -2,8 +2,10 @@ package flipnote.group.application.service;
 
 import org.springframework.stereotype.Service;
 
+import flipnote.group.adapter.out.entity.GroupEntity;
 import flipnote.group.application.port.in.CheckUserInGroupUseCase;
 import flipnote.group.application.port.out.GroupMemberRepositoryPort;
+import flipnote.group.application.port.out.GroupRepositoryPort;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -11,9 +13,12 @@ import lombok.RequiredArgsConstructor;
 public class CheckUserInGroupService implements CheckUserInGroupUseCase {
 
 	private final GroupMemberRepositoryPort groupMemberRepository;
+	private final GroupRepositoryPort groupRepository;
 
 	@Override
 	public boolean checkUserInGroup(Long userId, Long groupId) {
+
+		GroupEntity group = groupRepository.findById(groupId);
 
 		boolean isMember = groupMemberRepository.existsUserInGroup(groupId, userId);
 
