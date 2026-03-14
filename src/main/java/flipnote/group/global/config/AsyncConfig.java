1d4facfa5344cf50740e8b3aa01ebef6ab554862
dungbik <q1w2e30630@gmail.com>
2026-03-14 20:48:21 +0900

    Fix: 실수 수정

--- src/main/java/flipnote/group/global/config/AsyncConfig.java
+++ src/main/java/flipnote/group/global/config/AsyncConfig.java
@@ -1,11 +1,9 @@
 package flipnote.group.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@EnableRetry
 @Configuration
 public class AsyncConfig {
 }
