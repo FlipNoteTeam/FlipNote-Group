f10927c0bd7ccab2d244b7e9b1b97eba4a84bb56
이석범 <seokbeom0912@gmail.com>
2026-03-14 21:39:44 +0900

    Feat: 유저 조회시 닉네임 이미지 추가

--- src/main/java/flipnote/group/application/service/FindGroupMemberService.java
+++ src/main/java/flipnote/group/application/service/FindGroupMemberService.java
@@ -1,6 +1,9 @@
 package flipnote.group.application.service;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +15,11 @@ import flipnote.group.application.port.out.GroupMemberRepositoryPort;
 import flipnote.group.domain.model.member.MemberInfo;
 import flipnote.group.domain.policy.BusinessException;
 import flipnote.group.domain.policy.ErrorCode;
+import flipnote.user.grpc.GetUserResponse;
+import flipnote.user.grpc.GetUsersRequest;
+import flipnote.user.grpc.GetUsersResponse;
+import flipnote.user.grpc.UserQueryServiceGrpc;
+import io.grpc.StatusRuntimeException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -19,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class FindGroupMemberService implements FindGroupMemberUseCase {
 
 	private final GroupMemberRepositoryPort groupMemberRepository;
+	private final UserQueryServiceGrpc.UserQueryServiceBlockingStub userQueryService;
 
 	/**
 	 * 그룹 멤버 조회
@@ -36,8 +45,33 @@ public class FindGroupMemberService implements FindGroupMemberUseCase {
 		}
 
 		List<MemberInfo> memberInfoList = groupMemberRepository.findMemberInfo(cmd.groupId());
-		
-		//todo 멤버 정보에서 유저 아이디를 grpc를 통해 가져온 후 반환
+
+		List<Long> ids = memberInfoList.stream()
+			.map(MemberInfo::getUserId)
+			.collect(Collectors.toList());
+
+		GetUsersRequest req = GetUsersRequest.newBuilder()
+			.addAllUserIds(ids)
+			.build();
+
+		GetUsersResponse res;
+		try {
+			res = userQueryService.getUsers(req);
+		} catch (StatusRuntimeException e) {
+			throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+		}
+
+		// 맵으로 변환
+		Map<Long, GetUserResponse> userInfoMap = res.getUsersList().stream()
+			.collect(Collectors.toMap(GetUserResponse::getId, u -> u));
+
+		// memberInfo에 nickname, profileImage 업데이트
+		memberInfoList.forEach(memberInfo -> {
+			GetUserResponse userInfo = userInfoMap.get(memberInfo.getUserId());
+			if (userInfo != null) {
+				memberInfo.updateUserInfo(userInfo.getNickname(), userInfo.getProfileImageUrl());
+			}
+		});
 
 		return new FindGroupMemberResult(memberInfoList);
 	}
